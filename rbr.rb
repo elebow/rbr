2f65e12718bd6010e0de7ef4dd4c368ec5f1dc40
Eddie Lebow <elebow@users.noreply.github.com>
2018-02-02 01:23:14 -0500

    Break out some classes and generalize
    Use the convenience wrapper class Node instead of monkey-patching
    Parser::AST::Node.
    
    Store the matcher methods in a Matchers class that provides a single
    method to call.
    
    Generalize the way queries are handled so find_nodes doesn't need to
    know what kind of match is being performed.

--- rbr.rb
+++ rbr.rb
@@ -1,46 +1,28 @@
 require "parser/current"
+require "matchers"
+require "node"
 
-#Dirty POC of a semantically-aware code search tool for Ruby
+#A semantically-aware code search tool for Ruby
 #Usage:
 # search the file test/book.rb for statements that assign to the lvalue @author
-#   ruby rbr.rb assignment @author test/book.rb
+#   ruby -I . rbr.rb assignment @author test/book.rb
 
-#TODO https://www.justinweiss.com/articles/3-ways-to-monkey-patch-without-making-a-mess/
-module Parser
-  module AST
-    class Node
-      def assignment?
-        %i[lvasgn ivasgn cvasgn gvasgn casgn masgn].include? type
-      end
-    end
-  end
+def find_nodes(ast_node, query)
+  node = Node.new(ast_node)
+  return [] if node.nil?
+
+  node_matches = Matchers.match(node, query)
+  found_children = node.children.map { |child| find_nodes(child, query) }
+
+  (node_matches ? [node] : []) + found_children.flatten
 end
 
-def find_nodes(node:, name:, node_matcher:)
-  return [] unless node.is_a? Parser::AST::Node
-
-  found_children = node.children.map do |child|
-    find_nodes(node: child,
-               name: name,
-               node_matcher: ->(cur_node, n) { node_matcher.call(cur_node, n) })
-  end
-  this_node = node_matcher.call(node, name) ? [node] : []
-
-  this_node + found_children.flatten
-end
-
-def assignment_matcher(node, name)
-  node.assignment? && node.children.first == name
-end
-
-matchers = {
-  "assignment" => method(:assignment_matcher)
-}
-
+matcher = ARGV[0].to_sym
+lvalue = ARGV[1].to_sym
 root = Parser::CurrentRuby.parse(File.read(ARGV[2]))
-nodes = find_nodes(node: root,
-                   name: ARGV[1].to_sym,
-                   node_matcher: ->(cur_node, name) { matchers[ARGV[0]].call(cur_node, name) })
+
+nodes = find_nodes(root, matcher => { lvalue: lvalue })
+
 nodes.each do |node|
-  puts "#{node.loc.expression.line}: #{node.loc.expression.source}"
+  puts node.pretty_print
 end
